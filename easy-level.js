c09b6b27fc1f52ac0c077f963efbd170973b219a
LilyBishko <137831603+LilyBishko@users.noreply.github.com>
2023-09-26 18:52:07 +0000

    create

---
+++ easy-level.js
@@ -0,0 +1,8 @@
+/*
+document.querySelectorAll('.back-side').forEach(function(backSide) {
+    backSide.addEventListener('click', function() {
+        var memoryCard = this.closest('.memory-card');
+        memoryCard.classList.add('flip');
+    });
+}); */
+ const cards 
