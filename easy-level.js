3117016ee7890a9d4ac708a136d300f51a0553e9
LilyBishko <137831603+LilyBishko@users.noreply.github.com>
2023-09-28 18:43:28 +0000

    create

--- easy-level.js
+++ easy-level.js
@@ -1,8 +1,83 @@
-/*
-document.querySelectorAll('.back-side').forEach(function(backSide) {
-    backSide.addEventListener('click', function() {
-        var memoryCard = this.closest('.memory-card');
-        memoryCard.classList.add('flip');
-    });
-}); */
- const cards 
+const cards = document.querySelectorAll('.memory-card');
+
+ let hasFlippedCard = false;
+ let boardLocked = false;
+ let firstCard, secondCard;
+ let moves = 0; 
+
+
+
+const flipCard = e => {
+    if (boardLocked) return;
+   const target = e.target.parentElement;
+
+   if(target === firstCard) return;
+
+   target.classList.add("flip");
+  
+   function updateMovesDisplay() {
+    const movesDisplay = document.getElementById("moves");
+    movesDisplay.textContent = `Moves: ${moves}`;
+  }; 
+  
+   if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = target;
+    startTimer(); //fvnfknv
+   } else {
+    hasFlippedCard = false;
+    secondCard = target;
+    checkForMatch();
+    moves++; 
+    updateMovesDisplay(); 
+    checkForWin();
+   };
+};
+
+
+function updateMovesDisplay() {
+    const movesDisplay = document.getElementById("moves");
+    movesDisplay.textContent = `Moves: ${moves}`;
+  };
+  
+ 
+
+
+
+
+
+
+const checkForMatch = () => {
+   const isEqual = firstCard.dataset.picture === secondCard.dataset.picture;
+    isEqual ? disableCards() : unflipCards(); 
+};
+
+const disableCards = () => {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+};
+
+const unflipCards = () => {
+    boardLocked = true;
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        resetBoard();
+    }, 500 )
+};
+const resetBoard = () => {
+hasFlippedCard = boardLocked = false;
+firstCard = secondCard = false;
+};
+
+
+
+cards.forEach(card => {
+    card.addEventListener('click', flipCard)
+
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    card.style.order = randomIndex;
+});
+
+
+  
